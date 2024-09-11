431af44b2d870beeee90f94dfa238a9a9618d4f1
ElizaH2025 <eliza.horowitz2025@gbwl.org>
2024-09-11 10:15:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def main():
     is_fav_class = True   
     my_grade = 99.99 
 
+    # Check the type of variable
+    type_of_age = type(age)
+    print(tyoe_of_age)
+
+    # String formatting with f-strings
+    print(f"The tye of variable age is: {type _ofage}")
+
+
 
 if __name__ == "__main__":
     main()
