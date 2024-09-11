7d9588b6048f0bd434cb5d1b2a5065da0f9bbadc
ElizaH2025 <eliza.horowitz2025@gbwl.org>
2024-09-11 14:39:11 +0000

    edit main.py

--- main.py
+++ main.py
@@ -7,6 +7,39 @@ def main():
     is_fav_class = True   
     my_grade = 99.99 
 
+    # Check the type of variable
+    type_of_age = type(age)
+    print(type_of_age)
+
+    # String formatting with f-strings
+    print(f"The tye of variable age is: {type_of_age}")
+
+    # Declare Variables + Check Types
+    x = 42
+    type_of_x = type(x)
+    print(type_of_x)
+
+    y = 3/4
+    type_of_y = type(y)
+    print(type_of_y)
+
+    a = float(a)
+    type_of_a = type(a)
+    print(type_of_a)
+
+    name = "Nina"
+    type_of_name = type(name)
+    print(type_of_name)
+
+    print(f"My age in 10 years: ${age +10}")
+
+def bake_Cookie (ingredients, instructions, tempature): 
+    # Print the list of ingredients
+
+    # Print the oven tempature
+
+    # Print the instructions
+    print(instructions)
 
 if __name__ == "__main__":
     main()
