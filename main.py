3df3817956eb0c03ff75b7e6a9860f9568140ecc
ElizaH2025 <eliza.horowitz2025@gbwl.org>
2024-09-12 18:25:07 +0000

    edit main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print("hello world")
 
+    print( )
+
     # Declare variables 
     age = 17
     teacher = "Ms. Navab"
@@ -23,7 +25,7 @@ def main():
     type_of_y = type(y)
     print(type_of_y)
 
-    a = float(a)
+    a = float
     type_of_a = type(a)
     print(type_of_a)
 
@@ -31,15 +33,34 @@ def main():
     type_of_name = type(name)
     print(type_of_name)
 
-    print(f"My age in 10 years: ${age +10}")
+    print(f"My age in 10 years: {age +10}")
 
-def bake_Cookie (ingredients, instructions, tempature): 
+    print( )
+    
+    # Initialize args for function
+    ingredients_list =["chocolate chips", "cinnamons", "flour", "sugar", "butter", "eggs"]
+    snickerdoodle = "mix everything and put it in oven!"
+    temp = 300
+
+    # Call a function
+    bake_cookie(ingredients_list, snickerdoodle, temp) 
+
+    # Call a function with an optional argument 
+    bake_cookie (ingredients_list, snickerdoodle, temp, cutter = "heart")
+
+def bake_cookie (ingredients, instructions, tempature, cutter = "circle"): 
     # Print the list of ingredients
-
+    for item in ingredients: 
+        print(item)
+    
     # Print the oven tempature
+    print(f"Set the oven to {tempature} degrees Kelvin")
 
     # Print the instructions
-    print(instructions)
+    print(instructions, end="\n")
+
+    # Specify which cookie cutter to use
+    print(f"Now use a {cutter} cutter")
 
 if __name__ == "__main__":
     main()
